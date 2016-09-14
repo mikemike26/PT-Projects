69f9fd39472cde73a7b9dbaa1a7db5f04123bfcd
Mike Rensel <mike.rensel@mktec.com>
2016-09-14 18:30:29 -0400

    header styles

--- app/appHeader.component.ts
+++ app/appHeader.component.ts
@@ -7,7 +7,7 @@ import { Component } from '@angular/core';
         <h3>Project Time Site</h3>
       </header>
     `,
-    styles: ['h3 {text-align:center;}']
+    styles: ['h3 {margin:0; padding: 0.6em 0 0 0; text-align:center;} header {height: 3em; border-bottom: 2px solid #e5e5e5;}']
 })
 
 export class AppHeader { }
