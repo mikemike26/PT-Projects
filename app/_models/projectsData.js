4449cb9aeaed83c039393a9f448f8b88e8d73020
Mike Rensel <mike.rensel@mktec.com>
2016-09-17 12:40:48 -0400

    added updated properties

--- app/_models/projectsData.js
+++ app/_models/projectsData.js
@@ -23,10 +23,12 @@ var ProjectsData = (function () {
                 description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur ultricies rhoncus egestas. Proin a augue efficitur nulla porta gravida ut vitae est. Vestibulum tristique, felis vitae luctus placerat, erat turpis dignissim ante, at rhoncus lacus augue ac dui. Vivamus efficitur, mauris nec ornare sodales, erat erat congue neque, ut facilisis lectus lectus quis nisl. Aenean venenatis ipsum non lectus hendrerit facilisis. Aliquam erat volutpat. Sed semper vel tellus at sodales. In in mauris leo. Pellentesque id molestie sapien, non gravida metus.",
                 startDate: "Thu Sep 15 2016 21:01:57 GMT-0400 (EDT)",
                 endDate: "Thu Sep 25 2016 21:01:57 GMT-0400 (EDT)",
-                tasks: ["task1", "task2", "task3", "task4", "task5"],
-                issues: ["issue1", "issue2", "issue3", "issue4", "issue"],
                 members: ["Mike", "Ron", "Stefan"],
                 owner: "Michael Woods",
+                updatedBy: "",
+                updatedWhen: "",
+                tasks: ["task1", "task2", "task3", "task4", "task5"],
+                issues: ["issue1", "issue2", "issue3", "issue4", "issue"],
                 milestones: ["milestone1", "milestone2", "milestone3", "milestone4"]
             },
             {
@@ -37,9 +39,11 @@ var ProjectsData = (function () {
                 startDate: "Thu Sep 15 2016 21:01:57 GMT-0400 (EDT)",
                 endDate: "Thu Sep 25 2016 21:01:57 GMT-0400 (EDT)",
                 tasks: ["task1", "task2", "task3", "task4", "task5"],
-                issues: ["issue1", "issue2", "issue3", "issue4", "issue"],
                 members: ["Mike", "Ron", "Stefan"],
                 owner: "Michael Woods",
+                updatedBy: "",
+                updatedWhen: "",
+                issues: ["issue1", "issue2", "issue3", "issue4", "issue"],
                 milestones: ["milestone1", "milestone2", "milestone3", "milestone4"]
             }
         ];
